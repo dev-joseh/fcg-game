0dd6120c48d531b3129884e1533faff40463c201
gabrielcarvalhoavila <gabrielmachado50@hotmail.com>
2023-09-03 02:04:39 -0300

    colisoes com o cenario menos arvores e alguns ajustes

--- src/collisions.cpp
+++ src/collisions.cpp
@@ -1,34 +1,13 @@
 
-#include <iostream>
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>       /* time */
-
-// Headers abaixo são específicos de C++
-#include <map>
-#include <stack>
-#include <string>
-#include <vector>
-#include <limits>
-#include <fstream>
-#include <sstream>
-#include <stdexcept>
-#include <algorithm>
-
-// Headers das bibliotecas OpenGL
-#include <glad/glad.h>   // Criação de contexto OpenGL 3.3
-#include <GLFW/glfw3.h>  // Criação de janelas do sistema operacional
-
 // Headers da biblioteca GLM: criação de matrizes e vetores.
 #include <glm/mat4x4.hpp>
 #include <glm/vec4.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-// Headers da biblioteca para carregar modelos obj
-#include <tiny_obj_loader.h>
-#include <stb_image.h>
+// Arquivos "headers" padrões de C
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
 
 
 
